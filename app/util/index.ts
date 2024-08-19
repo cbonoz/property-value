30407a345857bb51e26c24ceb472952114f5e211
Chris Buonocore <chrisdistrict@gmail.com>
2024-08-19 00:06:58 -0400

    types

--- app/util/index.ts
+++ app/util/index.ts
@@ -1,4 +1,4 @@
-export const formatNumberWithCommas = (num: number) => {
-  const numString = parseFloat(num).toFixed(2)
+export const formatNumberWithCommas = (num: number | string) => {
+  const numString = parseFloat(num + "").toFixed(2)
   return numString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
